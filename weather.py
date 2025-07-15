fd1c93ee7caacdb05b1f2c4958974d0d3885b77e
PutinCake <zhiyemo09@gmail.com>
2025-07-15 10:32:48 -0700

    file update

--- weather.py
+++ weather.py
@@ -1,16 +1,16 @@
 import requests
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 from pprint import pprint
 
 
 load_dotenv()
 
 
-def get_current_weather():
-    print('\n*** Get Current Weather Conditions ***\n')
+def get_current_weather(city="Hayward"):
+    # print('\n*** Get Current Weather Conditions ***\n')
 
-    city = input('\nPlease enter the city name: \n')
+    # city = input('\nPlease enter the city name: \n')
 
     request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
 
@@ -18,13 +18,20 @@ def get_current_weather():
     print("API_KEY:", os.getenv("API_KEY"))
     weather_data = requests.get(request_url).json()
     # pprint(weather_data)
-
-    print(
-        f'Current weather for {weather_data["name"]}, {weather_data["sys"]["country"]}:')
-    print(f'Temperature: {weather_data["main"]["temp"]}°F')
-    print(
-        f'Feels like: {weather_data["main"]["feels_like"]}°F, and {weather_data["weather"][0]["description"]}.)')
+    return weather_data
+    # print(
+    #     f'Current weather for {weather_data["name"]}, {weather_data["sys"]["country"]}:')
+    # print(f'Temperature: {weather_data["main"]["temp"]}°F')
+    # print(
+    #     f'Feels like: {weather_data["main"]["feels_like"]}°F, and {weather_data["weather"][0]["description"]}.)')
 
 
 if __name__ == '__main__':
-    get_current_weather()
+    print('\n*** Get Current Weather Conditions ***\n')
+    city = input('\nPlease enter the city name: \n')
+
+    if not bool(city.strip()):
+        city = "Hayward"
+    weather_data = get_current_weather(city)
+    print("\n")
+    pprint(weather_data)
