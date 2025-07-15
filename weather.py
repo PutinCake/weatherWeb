99f9b12cca07d417270122bf782b7e724810a3dc
PutinCake <zhiyemo09@gmail.com>
2025-07-14 17:19:25 -0700

    Initial commit

---
+++ weather.py
@@ -0,0 +1,30 @@
+import requests
+from dotenv import load_dotenv
+import os
+from pprint import pprint
+
+
+load_dotenv()
+
+
+def get_current_weather():
+    print('\n*** Get Current Weather Conditions ***\n')
+
+    city = input('\nPlease enter the city name: \n')
+
+    request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+
+    # print(request_url)
+    print("API_KEY:", os.getenv("API_KEY"))
+    weather_data = requests.get(request_url).json()
+    # pprint(weather_data)
+
+    print(
+        f'Current weather for {weather_data["name"]}, {weather_data["sys"]["country"]}:')
+    print(f'Temperature: {weather_data["main"]["temp"]}°F')
+    print(
+        f'Feels like: {weather_data["main"]["feels_like"]}°F, and {weather_data["weather"][0]["description"]}.)')
+
+
+if __name__ == '__main__':
+    get_current_weather()
